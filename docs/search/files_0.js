d384198b806ce05a9abfba5df793161ca4919514
voidptrnull <voidptrnull@proton.me>
2024-09-22 19:03:39 +0530

    update docs.

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -3,7 +3,8 @@ var searchData=
   ['cdlinkedlist_2eh_0',['CDLinkedList.h',['../CDLinkedList_8h.html',1,'']]],
   ['cerror_2eh_1',['CError.h',['../CError_8h.html',1,'']]],
   ['chashmap_2eh_2',['CHashMap.h',['../CHashMap_8h.html',1,'']]],
-  ['cresult_2eh_3',['CResult.h',['../CResult_8h.html',1,'']]],
-  ['cstring_2eh_4',['CString.h',['../CString_8h.html',1,'']]],
-  ['cvector_2eh_5',['CVector.h',['../CVector_8h.html',1,'']]]
+  ['chrtime_2eh_3',['CHRTime.h',['../CHRTime_8h.html',1,'']]],
+  ['cresult_2eh_4',['CResult.h',['../CResult_8h.html',1,'']]],
+  ['cstring_2eh_5',['CString.h',['../CString_8h.html',1,'']]],
+  ['cvector_2eh_6',['CVector.h',['../CVector_8h.html',1,'']]]
 ];
