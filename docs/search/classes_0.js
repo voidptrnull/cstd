d54699d74b13e5b6e6d80283c7c6923dafa3231f
voidptrnull <voidptrnull@proton.me>
2024-12-08 19:52:41 +0530

    Bump and update documentation.

--- docs/search/classes_0.js
+++ docs/search/classes_0.js
@@ -3,8 +3,9 @@ var searchData=
   ['cerror_0',['CError',['../structCError.html',1,'']]],
   ['chashmap_1',['CHashMap',['../structCHashMap.html',1,'']]],
   ['chashset_2',['CHashSet',['../structCHashSet.html',1,'']]],
-  ['cresult_3',['CResult',['../structCResult.html',1,'']]],
-  ['cstack_4',['CStack',['../structCStack.html',1,'']]],
-  ['cstring_5',['CString',['../structCString.html',1,'']]],
-  ['cvector_6',['CVector',['../structCVector.html',1,'']]]
+  ['clinkedlist_3',['CLinkedList',['../structCLinkedList.html',1,'']]],
+  ['cresult_4',['CResult',['../structCResult.html',1,'']]],
+  ['cstack_5',['CStack',['../structCStack.html',1,'']]],
+  ['cstring_6',['CString',['../structCString.html',1,'']]],
+  ['cvector_7',['CVector',['../structCVector.html',1,'']]]
 ];
--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -4,10 +4,11 @@ var searchData=
   ['chashmap_2eh_1',['CHashMap.h',['../CHashMap_8h.html',1,'']]],
   ['chashset_2eh_2',['CHashSet.h',['../CHashSet_8h.html',1,'']]],
   ['chrtime_2eh_3',['CHRTime.h',['../CHRTime_8h.html',1,'']]],
-  ['clog_2eh_4',['CLog.h',['../CLog_8h.html',1,'']]],
-  ['cresult_2eh_5',['CResult.h',['../CResult_8h.html',1,'']]],
-  ['cstack_2eh_6',['CStack.h',['../CStack_8h.html',1,'']]],
-  ['cstd_2eh_7',['CSTD.h',['../CSTD_8h.html',1,'']]],
-  ['cstring_2eh_8',['CString.h',['../CString_8h.html',1,'']]],
-  ['cvector_2eh_9',['CVector.h',['../CVector_8h.html',1,'']]]
+  ['clinkedlist_2eh_4',['CLinkedList.h',['../CLinkedList_8h.html',1,'']]],
+  ['clog_2eh_5',['CLog.h',['../CLog_8h.html',1,'']]],
+  ['cresult_2eh_6',['CResult.h',['../CResult_8h.html',1,'']]],
+  ['cstack_2eh_7',['CStack.h',['../CStack_8h.html',1,'']]],
+  ['cstd_2eh_8',['CSTD.h',['../CSTD_8h.html',1,'']]],
+  ['cstring_2eh_9',['CString.h',['../CString_8h.html',1,'']]],
+  ['cvector_2eh_10',['CVector.h',['../CVector_8h.html',1,'']]]
 ];
